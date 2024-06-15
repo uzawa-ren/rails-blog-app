df3dd8130dabca1d41e6bfd6ce29f71bf7fb7829
Uzawa Ren <96781771+uzawa-ren@users.noreply.github.com>
2024-06-15 16:58:44 +0300

    Add controller actions for article updating

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,6 +20,20 @@ class ArticlesController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
   
   private
 
